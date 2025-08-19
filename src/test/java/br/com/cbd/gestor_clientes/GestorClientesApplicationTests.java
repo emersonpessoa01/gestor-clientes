3d2e7ba4554e6f0c270645ab02699824e421a7bf
emersonpessoa <eps011108@gmail.com>
2025-08-19 12:39:26 -0300

    feat: Configurações iniciais e criado entity :sparkler:

---
+++ src/test/java/br/com/cbd/gestor_clientes/GestorClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cbd.gestor_clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
