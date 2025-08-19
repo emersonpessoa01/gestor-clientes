a3f994687ff17ac408de127e620337b9f3e2dbe7
emersonpessoa <eps011108@gmail.com>
2025-08-19 14:13:43 -0300

    feat: Adicionado CreateClienteDTO :rocket:

--- src/main/java/br/com/cbd/gestor_clientes/dto/CreateClienteDTO.java
+++ src/main/java/br/com/cbd/gestor_clientes/dto/CreateClienteDTO.java
@@ -1,4 +1,31 @@
 package br.com.cbd.gestor_clientes.dto;
 
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Data
 public class CreateClienteDTO {
+    @NotBlank(message = "Nome é obrigatório")
+    @Size(min = 3, message = "Nome deve ter no mínimo 3 caracteres")
+    private String nome;
+
+    @NotBlank(message = "Email é obrigatório")
+    @Email(message = "Email inválido")
+    private String email;
+
+    private String telefone;
+
+    @NotBlank(message = "CPF é obrigatório")
+    private String cpf;
+
+    @NotNull
+    private String status = "ATIVO";
 }
