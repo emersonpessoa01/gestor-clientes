eaa1ddd67cd9aaba212e622657a0e43c233ab8b2
emersonpessoa <eps011108@gmail.com>
2025-08-19 12:46:22 -0300

    feat:Criado ClienteDTO :rocket:

---
+++ src/main/java/br/com/cbd/gestor_clientes/dto/ClienteDTO.java
@@ -0,0 +1,16 @@
+package br.com.cbd.gestor_clientes.dto;
+
+
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Data
+public class ClienteDTO {
+    private Long id;
+    private String nome;
+    private String email;
+    private String telefone;
+    private String cpf;
+    private LocalDateTime criadoEm;
+    private LocalDateTime atualizadoEm;
+}
