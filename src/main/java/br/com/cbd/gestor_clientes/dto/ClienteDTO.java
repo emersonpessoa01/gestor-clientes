0a702c5ad0d9e8d588801710b7d682d3ef2c4767
emersonpessoa <eps011108@gmail.com>
2025-08-20 12:39:11 -0300

    feat(dto): Adicionado no ClienteDTO anotações OpenAPIl :rocket:

--- src/main/java/br/com/cbd/gestor_clientes/dto/ClienteDTO.java
+++ src/main/java/br/com/cbd/gestor_clientes/dto/ClienteDTO.java
@@ -1,17 +1,33 @@
 package br.com.cbd.gestor_clientes.dto;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jdk.jfr.Description;
 import lombok.Data;
 import java.time.LocalDateTime;
 
 @Data
 public class ClienteDTO {
     private Long id;
+
+    @Schema(description = "Nome do cliente", example = "João Silva")
     private String nome;
+
+    @Schema(description = "Email do cliente", example = "meuemail@mail.com")
     private String email;
+
+    @Schema(description = "Telefone do cliente", example = "+55 (xx) xxxxx-xxxx")
     private String telefone;
+
+    @Schema(description = "CPF do cliente", example = "xxx.xxx.xxx-xx")
     private String cpf;
+
+    @Schema(description = "Status do cliente", example = "ATIVO | INATIVO | PROSPECT")
     private String status;
+
+    @Schema(description = "Data da criação", example = "2025-01-15")
     private LocalDateTime criadoEm;
+
+    @Schema(description = "Data da última atualização", example = "2025-04-10")
     private LocalDateTime atualizadoEm;
 }
