0e14953a467913bbcbbb52c6663deb7884a0ed60
emersonpessoa <eps011108@gmail.com>
2025-08-20 12:10:45 -0300

    feat(dto): Adicionado no CreateClienteDTO anotações OpenAPI :rocket:

--- src/main/java/br/com/cbd/gestor_clientes/dto/CreateClienteDTO.java
+++ src/main/java/br/com/cbd/gestor_clientes/dto/CreateClienteDTO.java
@@ -1,31 +1,33 @@
 package br.com.cbd.gestor_clientes.dto;
 
-import jakarta.persistence.Column;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
 
-import java.time.LocalDateTime;
 
 @Data
 public class CreateClienteDTO {
     @NotBlank(message = "Nome é obrigatório")
     @Size(min = 3, message = "Nome deve ter no mínimo 3 caracteres")
+    @Schema(description = "Nome do cliente", example = "João Silva")
     private String nome;
 
     @NotBlank(message = "Email é obrigatório")
     @Email(message = "Email inválido")
+    @Schema(description = "Email do cliente", example = "meuemail@gmail.com")
     private String email;
 
+    @Schema(description = "Telefone do usuário", example = "+55 (xx) xxxxx-xxxx")
     private String telefone;
 
     @NotBlank(message = "CPF é obrigatório")
+    @Schema(description = "CPF do cliente", example = "999.999.999-00")
     private String cpf;
 
     @NotNull
+    @Schema(description = "Status do cliente", example = "ATIVO | INATIVO | PROSPECT")
     private String status = "ATIVO";
 }
