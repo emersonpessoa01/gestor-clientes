4f3d2514ce601051fc21c537884a67354d42393e
emersonpessoa <eps011108@gmail.com>
2025-08-20 19:48:08 -0300

    feat: Adicionado @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss") aos campos criadoEm e atualizadoEm no ClienteDTO para formatar datas no JSON :rocket:

--- src/main/java/br/com/cbd/gestor_clientes/dto/ClienteDTO.java
+++ src/main/java/br/com/cbd/gestor_clientes/dto/ClienteDTO.java
@@ -1,6 +1,7 @@
 package br.com.cbd.gestor_clientes.dto;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jdk.jfr.Description;
 import lombok.Data;
@@ -26,8 +27,10 @@ public class ClienteDTO {
     private String status;
 
     @Schema(description = "Data da criação", example = "2025-01-15")
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
     private LocalDateTime criadoEm;
 
     @Schema(description = "Data da última atualização", example = "2025-04-10")
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
     private LocalDateTime atualizadoEm;
 }
