6674a1e0b8eaa39880942b9d00869b2e9ff182b2
emersonpessoa <eps011108@gmail.com>
2025-08-20 11:49:27 -0300

    docs: iIncluido metadata OpenAPI (title, version, description) :tada:

--- src/main/java/br/com/cbd/gestor_clientes/GestorClientesApplication.java
+++ src/main/java/br/com/cbd/gestor_clientes/GestorClientesApplication.java
@@ -1,9 +1,12 @@
 package br.com.cbd.gestor_clientes;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Gestor Clientes API", version = "1.0", description = "Api de Gestor de Clientes"))
 public class GestorClientesApplication {
 
 	public static void main(String[] args) {
