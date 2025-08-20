8bbf7ef4edb8d5c287c7e32d79ca7cacaf1becb1
emersonpessoa <eps011108@gmail.com>
2025-08-20 12:36:01 -0300

    feat(dto): Adicionado no UpdatedClienteDTO anotações OpenAPI :rocket:

--- src/main/java/br/com/cbd/gestor_clientes/dto/UpdateClienteDTO.java
+++ src/main/java/br/com/cbd/gestor_clientes/dto/UpdateClienteDTO.java
@@ -1,5 +1,6 @@
 package br.com.cbd.gestor_clientes.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -10,14 +11,19 @@ import lombok.Data;
 public class UpdateClienteDTO {
     @NotBlank(message = "Nome é obrigatório")
     @Size(min=3,message = "Nome dever ter no mínimo 3 caracteres")
+    @Schema(description = "Nome do cliente", example = "João Silva")
     private String nome;
 
     @NotBlank(message = "Email é obrigatório")
     @Email(message = "Email inválido")
+    @Schema(description = "Email do cliente", example = "meuemail@gmail.com")
     private String email;
 
+    @Schema(description = "Telefone do cliente", example = "+55 (xx) xxxxx-xxxx")
     private String telefone;
+
     @NotNull
+    @Schema(description = "Status do cliente", example = "ATIVO | INATIVO | PROSPECT")
     private String status;
 
 }
