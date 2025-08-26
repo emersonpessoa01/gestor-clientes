c509b444fa0f9c5398e15cf8ade6193e212e047c
emersonpessoa <eps011108@gmail.com>
2025-08-26 11:09:45 -0300

    feat: Criado ClienteResponse :rocket:

---
+++ src/main/java/br/com/cbd/gestor_clientes/adapter/in/web/ClienteResponse.java
@@ -0,0 +1,92 @@
+package br.com.cbd.gestor_clientes.adapter.in.web;
+
+import java.time.LocalDateTime;
+
+public class ClienteResponse {
+    private Long id;
+    private String nome;
+    private String email;
+    private String telefone;
+    private String cpf;
+    private String status;
+    private LocalDateTime criadoEm;
+    private LocalDateTime atualizadoEm;
+
+    // Construtores, getters e setters
+    public ClienteResponse() {
+    }
+    public ClienteResponse(Long id, String nome, String email, String telefone, String cpf, String status, LocalDateTime criadoEm, LocalDateTime atualizadoEm) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+        this.cpf = cpf;
+        this.status = status;
+        this.criadoEm = criadoEm;
+        this.atualizadoEm = atualizadoEm;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getCriadoEm() {
+        return criadoEm;
+    }
+
+    public void setCriadoEm(LocalDateTime criadoEm) {
+        this.criadoEm = criadoEm;
+    }
+
+    public LocalDateTime getAtualizadoEm() {
+        return atualizadoEm;
+    }
+
+    public void setAtualizadoEm(LocalDateTime atualizadoEm) {
+        this.atualizadoEm = atualizadoEm;
+    }
+}
