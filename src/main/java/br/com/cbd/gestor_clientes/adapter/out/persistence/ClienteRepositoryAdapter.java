56fa372383942eb395703f256bb881e37b576a9c
emersonpessoa <eps011108@gmail.com>
2025-08-26 12:44:56 -0300

    feat: Removido imports sem uso :sparkler:

--- src/main/java/br/com/cbd/gestor_clientes/adapter/out/persistence/ClienteRepositoryAdapter.java
+++ src/main/java/br/com/cbd/gestor_clientes/adapter/out/persistence/ClienteRepositoryAdapter.java
@@ -7,10 +7,7 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
-
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
