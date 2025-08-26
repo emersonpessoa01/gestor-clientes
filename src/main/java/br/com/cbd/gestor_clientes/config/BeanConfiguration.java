ead5818fa9afd197ff1a537e30b4ee8d5c28cc5c
emersonpessoa <eps011108@gmail.com>
2025-08-26 11:14:28 -0300

    feat: Criado BeanConfiguration :rocket:

---
+++ src/main/java/br/com/cbd/gestor_clientes/config/BeanConfiguration.java
@@ -0,0 +1,14 @@
+package br.com.cbd.gestor_clientes.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import javax.sql.DataSource;
+
+@Configuration
+public class BeanConfiguration {
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+}
