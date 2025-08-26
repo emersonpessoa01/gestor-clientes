a9643c1ed963ef18ff21631c3e9631dfcc4fd358
emersonpessoa <eps011108@gmail.com>
2025-08-26 11:12:45 -0300

    feat: Criado CriarClienteUseCase :rocket:

---
+++ src/main/java/br/com/cbd/gestor_clientes/core/port/in/CriarClienteUseCase.java
@@ -0,0 +1,7 @@
+package br.com.cbd.gestor_clientes.core.port.in;
+
+import br.com.cbd.gestor_clientes.core.domain.model.Cliente;
+
+public interface CriarClienteUseCase {
+    Cliente execute(Cliente cliente);
+}
