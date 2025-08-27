aa0e26ca2f65fe25456d20f968d94f32daa58519
emersonpessoa <eps011108@gmail.com>
2025-08-27 19:41:45 -0300

    feat: Ajustado endpoint /validate-cpf no ClienteController para validar CPF :rocket:

--- src/main/java/br/com/cbd/gestor_clientes/adapter/in/web/ClienteController.java
+++ src/main/java/br/com/cbd/gestor_clientes/adapter/in/web/ClienteController.java
@@ -6,8 +6,9 @@ import br.com.cbd.gestor_clientes.core.port.in.AtualizarClienteUseCase;
 import br.com.cbd.gestor_clientes.core.port.in.DeletarClienteUseCase;
 import br.com.cbd.gestor_clientes.core.port.in.BuscarClienteUseCase;
 import br.com.cbd.gestor_clientes.core.port.in.ListarClienteUseCase;
+import br.com.cbd.gestor_clientes.core.service.ClienteService;
 import br.com.cbd.gestor_clientes.mapper.ClienteMapper;
-import jakarta.websocket.ClientEndpoint;
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,16 +24,18 @@ public class ClienteController {
     private final BuscarClienteUseCase buscarUseCase;
     private final ListarClienteUseCase listarUseCase;
     private final ClienteMapper mapper;
+    private final ClienteService clienteService;
 
     public ClienteController(CriarClienteUseCase criarUseCase, AtualizarClienteUseCase atualizarUseCase,
                              DeletarClienteUseCase deletarUseCase, BuscarClienteUseCase buscarUseCase,
-                             ListarClienteUseCase listarUseCase, ClienteMapper mapper) {
+                             ListarClienteUseCase listarUseCase, ClienteMapper mapper, ClienteService clienteService) {
         this.criarUseCase = criarUseCase;
         this.atualizarUseCase = atualizarUseCase;
         this.deletarUseCase = deletarUseCase;
         this.buscarUseCase = buscarUseCase;
         this.listarUseCase = listarUseCase;
         this.mapper = mapper;
+        this.clienteService = clienteService;
     }
 
     @PostMapping
@@ -67,5 +70,15 @@ public class ClienteController {
         List<Cliente> clientes = listarUseCase.findAll();
         List<ClienteResponse> responseList = mapper.toResponseList(clientes); // Assume que ClienteMapper tem toResponseList
         return ResponseEntity.ok(responseList);
+    }
+    @GetMapping("/validate-cpf")
+    public ResponseEntity<String> validateCpf(
+            @Parameter(description = "CPF a validar(ex: 111.444.777-35)",required =true, example = "111.444.777-35") @RequestParam String cpf
+    ){
+        if(cpf == null || cpf.isEmpty()){
+            return ResponseEntity.badRequest().body("CPF não pode ser vazio.");
+        }
+    boolean valido = clienteService.validarCpf(cpf);
+        return ResponseEntity.ok(valido ? "CPF válido." : "CPF inválido.");
     }
 }
