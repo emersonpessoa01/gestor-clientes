ff05b40b4c5be8607637ed097310cef4ad4e3860
emersonpessoa <eps011108@gmail.com>
2025-08-27 12:39:29 -0300

    fix: Adicionado toResponseList e ajustado mapeamento em ClienteMapper :wrench:

---
+++ src/main/java/br/com/cbd/gestor_clientes/core/port/in/ListarClienteUseCase.java
@@ -0,0 +1,9 @@
+package br.com.cbd.gestor_clientes.core.port.in;
+
+import br.com.cbd.gestor_clientes.core.domain.model.Cliente;
+
+import java.util.List;
+
+public interface ListarClienteUseCase {
+    List<Cliente> findAll();
+}
