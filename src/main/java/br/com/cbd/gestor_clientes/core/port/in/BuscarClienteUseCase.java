d30b125f020e48a0cf5acda8255a3b48f88f88c9
emersonpessoa <eps011108@gmail.com>
2025-08-27 12:35:11 -0300

    feat: Adicionado tratamento de exceção em BuscarClienteUseCase :sparkler:

--- src/main/java/br/com/cbd/gestor_clientes/core/port/in/BuscarClienteUseCase.java
+++ src/main/java/br/com/cbd/gestor_clientes/core/port/in/BuscarClienteUseCase.java
@@ -5,5 +5,5 @@ import br.com.cbd.gestor_clientes.core.domain.model.Cliente;
 import java.util.Optional;
 
 public interface BuscarClienteUseCase {
-    Optional<Cliente> execute(Long id);
+    Optional<Cliente> findById(Long id);
 }
