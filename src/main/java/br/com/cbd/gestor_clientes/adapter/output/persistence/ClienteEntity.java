a5d2a5eaed302466a72505df0e19140bc6f340ec
emersonpessoa <eps011108@gmail.com>
2025-09-02 21:50:22 -0300

    feat: Criado ClienteEntity :rocket:

---
+++ src/main/java/br/com/cbd/gestor_clientes/adapter/output/persistence/ClienteEntity.java
@@ -0,0 +1,84 @@
+package br.com.cbd.gestor_clientes.adapter.output.persistence;
+
+import java.time.LocalDateTime;
+
+public class ClienteEntity {
+    private Long id;
+    private String nome;
+    private String email;
+    private String telefone;
+    private String cpf;
+    private String status;
+    private LocalDateTime criadoEm;
+    private LocalDateTime atualizadoEm;
+
+    // Construtores, getters e setters
+    public ClienteEntity() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getCriadoEm() {
+        return criadoEm;
+    }
+
+    public void setCriadoEm(LocalDateTime criadoEm) {
+        this.criadoEm = criadoEm;
+    }
+
+    public LocalDateTime getAtualizadoEm() {
+        return atualizadoEm;
+    }
+
+    public void setAtualizadoEm(LocalDateTime atualizadoEm) {
+        this.atualizadoEm = atualizadoEm;
+    }
+
+}
