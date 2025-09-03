4b6370ad3f5285bc8e2bea68fd08d5c7a7fd5b3d
emersonpessoa <eps011108@gmail.com>
2025-09-02 21:52:21 -0300

    fix: Renomeado ClienteRepositoryPort para ClienteOutPort :wrench:

---
+++ src/main/java/br/com/cbd/gestor_clientes/core/port/output/ClienteOutputPort.java
@@ -0,0 +1,22 @@
+package br.com.cbd.gestor_clientes.core.port.output;
+
+import br.com.cbd.gestor_clientes.core.domain.model.Cliente;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ClienteOutputPort {
+    Cliente save(Cliente cliente);
+
+    Optional<Cliente> findById(Long id);
+
+    List<Cliente> findAll();
+
+    Cliente update(Cliente cliente);
+
+    void delete(Long id);
+
+    boolean existsByCpf(String cpf); // Para validar duplicidade de CPF
+
+    boolean existsByEmail(String email); // Para validar duplicidade de Email
+}
