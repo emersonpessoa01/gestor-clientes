c08a9cb0df97adac6336a57b8541d162d0ea5ae3
emersonpessoa <eps011108@gmail.com>
2025-09-02 22:39:06 -0300

    fix: BeanConfiguration movido para infrastructure :wrench:

--- src/main/java/br/com/cbd/gestor_clientes/infrastructure/config/BeanConfiguration.java
+++ src/main/java/br/com/cbd/gestor_clientes/infrastructure/config/BeanConfiguration.java
@@ -1,4 +1,4 @@
-package br.com.cbd.gestor_clientes.config;
+package br.com.cbd.gestor_clientes.infrastructure.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
