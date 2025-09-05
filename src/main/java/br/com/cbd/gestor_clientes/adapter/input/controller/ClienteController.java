3eb9e482d0d00e87a4e63de5c52608d0d3a93a1d
emersonpessoa <eps011108@gmail.com>
2025-09-05 17:20:08 -0300

    fix: Imports do dto atualizado no ClienteController :wrench:

--- src/main/java/br/com/cbd/gestor_clientes/adapter/input/controller/ClienteController.java
+++ src/main/java/br/com/cbd/gestor_clientes/adapter/input/controller/ClienteController.java
@@ -2,8 +2,8 @@ package br.com.cbd.gestor_clientes.adapter.input.controller;
 
 import br.com.cbd.gestor_clientes.application.port.input.ClienteInputPort;
 import br.com.cbd.gestor_clientes.adapter.input.mapper.ClienteMapper;
-import br.com.cbd.gestor_clientes.adapter.input.request.ClienteRequest;
-import br.com.cbd.gestor_clientes.adapter.input.request.ClienteResponse;
+import br.com.cbd.gestor_clientes.adapter.input.dto.ClienteRequest;
+import br.com.cbd.gestor_clientes.adapter.input.dto.ClienteResponse;
 import br.com.cbd.gestor_clientes.core.model.Cliente;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
