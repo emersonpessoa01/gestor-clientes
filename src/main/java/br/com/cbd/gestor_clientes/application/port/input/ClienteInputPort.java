07196d1f3d7ae90982acccde725acda5510f734b
emersonpessoa <eps011108@gmail.com>
2025-09-05 12:52:33 -0300

    fix: Recommitado a interface ClienteInputPort :wrench:

---
+++ src/main/java/br/com/cbd/gestor_clientes/application/port/input/ClienteInputPort.java
@@ -0,0 +1,15 @@
+package br.com.cbd.gestor_clientes.application.port.input;
+
+import br.com.cbd.gestor_clientes.core.model.Cliente;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ClienteInputPort {
+    Cliente create(Cliente cliente);
+    Cliente update(Long id, Cliente cliente);
+    void delete(Long id);
+    Optional<Cliente> findById(Long id);
+    List<Cliente> findAll();
+    boolean validarCpf(String cpf);
+}
