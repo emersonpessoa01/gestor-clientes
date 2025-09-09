eb912c19d743ae6ef4c16466472d33eeb86edcdd
emersonpessoa <eps011108@gmail.com>
2025-09-08 23:26:10 -0300

    feat: Atualizado function_contar_clientes_ativos.sql :rocket:

--- src/main/resources/function_contar_clientes_ativos.sql
+++ src/main/resources/function_contar_clientes_ativos.sql
@@ -1,12 +1,12 @@
 -- Função para contar clientes ativos
-CREATE OR REPLACE FUNCTION public.contar_clientes_ativos()
+create or replace function public.contar_clientes_ativos()
 RETURNS INTEGER AS $$
-DECLARE
-    total INTEGER;
-BEGIN
-    SELECT COUNT(*) INTO total
-    FROM cliente
-    WHERE status = 'ATIVO';
-    RETURN total;
-END;
+declare
+    total integer;
+begin
+    select count(*) into total
+    from cliente
+    where status = 'ATIVO';
+    return total;
+end;
 $$ LANGUAGE plpgsql;
