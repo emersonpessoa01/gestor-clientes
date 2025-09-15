d2b42e28ab5098c3f05f5319d9d6eaa65f25309f
emersonpessoa <eps011108@gmail.com>
2025-09-15 13:15:20 -0300

    feat: ClienteMapper atualizado :rocket:

--- src/main/java/br/com/cbd/gestor_clientes/adapter/input/mapper/ClienteMapper.java
+++ src/main/java/br/com/cbd/gestor_clientes/adapter/input/mapper/ClienteMapper.java
@@ -2,11 +2,11 @@ package br.com.cbd.gestor_clientes.adapter.input.mapper;
 
 import java.util.List;
 
+import br.com.cbd.gestor_clientes.adapter.input.response.ClienteResponse;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 import br.com.cbd.gestor_clientes.adapter.input.request.ClienteRequest;
-import br.com.cbd.gestor_clientes.adapter.input.request.ClienteResponse;
 import br.com.cbd.gestor_clientes.core.domain.model.Cliente;
 
 @Mapper(componentModel = "spring")
