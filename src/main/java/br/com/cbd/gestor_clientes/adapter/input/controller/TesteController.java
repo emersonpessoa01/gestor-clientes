bb058dcffea37fca0aa108a66f0506916b19509f
emersonpessoa <eps011108@gmail.com>
2025-10-24 11:43:46 -0300

    test: Adicionado dois endpoints para testar no GlobalExceptionHandlerTest :hammer:

--- src/main/java/br/com/cbd/gestor_clientes/adapter/input/controller/TesteController.java
+++ src/main/java/br/com/cbd/gestor_clientes/adapter/input/controller/TesteController.java
@@ -1,5 +1,6 @@
 package br.com.cbd.gestor_clientes.adapter.input.controller;
 
+import br.com.cbd.gestor_clientes.adapter.input.exception.BusinessException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,5 +12,14 @@ public class TesteController {
     @GetMapping
     public String ping() {
         return "pong";
+    }
+    @GetMapping("/erro-negocio")
+    public void erroNegocio() {
+        throw new BusinessException("Erro de negócio simulado");
+    }
+
+    @GetMapping("/erro-generico")
+    public void erroGenerico() {
+        throw new RuntimeException("Erro genérico simulado");
     }
 }
